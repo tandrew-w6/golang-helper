57e1897cb8a81eb504de6a5df8a9c5a1b91eae43
tandrew-w6 <77832757+tandrew-w6@users.noreply.github.com>
2023-11-10 10:27:08 +0700

    Add convert uint32

--- lib/utils/converter/int.go
+++ lib/utils/converter/int.go
@@ -15,3 +15,19 @@ func ConvertInt32ToPointer(value int32) *int32 {
 
 	return result
 }
+
+func ConvertPointerToUint32(value *uint32) uint32 {
+	var result uint32
+
+	if value != nil {
+		result = *value
+	}
+
+	return result
+}
+
+func ConvertUint32ToPointer(value uint32) *uint32 {
+	var result *uint32 = &value
+
+	return result
+}
